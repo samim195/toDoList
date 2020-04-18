a6897f2a34b8464d84573e45d2d9dba22a601ba1
samimkohistani <samimkohistani@hotmail.com>
2020-04-18 16:07:01 +0100

    Add gitingore, procfile and update ports

--- app.js
+++ app.js
@@ -162,9 +162,13 @@ app.post("/work", function(req, res) {
     res.redirect("/work");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(port, function() {
+    console.log("Server has started succesfully");
 });
 
 app.get("/about", function(req, res) {
